bb48e75f868cbb5a971261d328ce49b5946d83b7
noumanbaloch <noumanbaloch@NOUMANBALOCEBED>
2021-06-02 22:20:38 +0500

    Initial Commit

---
+++ ShopIn/ShopIn.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopIn.WebUI.Startup))]
+namespace ShopIn.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
